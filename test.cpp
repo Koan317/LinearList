c215c30f7918bc8b5cf243ba5fa94c0dc918241a
Koan <koan317@qq.com>
2023-10-07 17:03:49 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,5 +3,14 @@
 
 int main() {
 	SeqList<int> s1(10);
+	s1.input();
+	s1.output();
+	SeqList<int> s2(4);
+	void* tempptr = &s2;
+	cout << "qwer " << sizeof(tempptr) << endl;
+	s2 = s1;
+	cout << "qwer " << sizeof(tempptr) << endl;
+	s2.output();
+	s1.sort();
 	s1.output();
 }
